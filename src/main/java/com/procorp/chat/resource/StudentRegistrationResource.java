2c486e933d812cbbc83de898043d95831c1823e8
abhitman <abhishekkumar444@gmail.com>
2023-09-25 16:17:02 +0530

    student rest APIs

--- src/main/java/com/procorp/chat/resource/StudentRegistrationResource.java
+++ src/main/java/com/procorp/chat/resource/StudentRegistrationResource.java
@@ -1,16 +1,17 @@
 package com.procorp.chat.resource;
 
 import com.procorp.chat.dtos.StudentDTO;
+import com.procorp.chat.entities.Student;
 import com.procorp.chat.service.StudentService;
 
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("member-service")
@@ -26,4 +27,31 @@ public class StudentRegistrationResource {
         return "Student with Name:" + student.getStudentName() + " has been Added.";
     }
 
+
+    @DeleteMapping("/student/{studentId}")
+    public String removeStudent(Long studentId) {
+        studentService.removeStudent(studentId);
+        return "Student with Id:" + studentId + " has been removed.";
+    }
+
+    @GetMapping("/getStudentsById/{studentId}")
+    public Student getStudentsById(@PathVariable Long studentId) {
+        return studentService.getStudentById(studentId);
+    }
+
+    @GetMapping("/getAllStudents")
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+//	@PutMapping("/student")
+//	public List<Student> updateStudent(@Valid @RequestBody Student student) {
+//		return studentService.updateStudent();
+//	}
+
+    @PutMapping("/student")
+    public Long updateStudent(@javax.validation.Valid @RequestBody StudentDTO student) {
+        return studentService.updateStudent(student);
+    }
 }
+
