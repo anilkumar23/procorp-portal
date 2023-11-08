5a67b9baf46829def6a83f74038c426a951f2dbf
chowdeswararaomahipathi <chowdeswararao.mahipathi123@gmail.com>
2023-11-08 23:39:30 +0530

    gg

--- order-service/src/main/java/com/procorp/ordermanagement/entities/Inventory_Audit.java
+++ order-service/src/main/java/com/procorp/ordermanagement/entities/Inventory_Audit.java
@@ -1,2 +1,27 @@
-package com.procorp.ordermanagement.entities;public class Inventory_Audit {
+package com.procorp.ordermanagement.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "inventory_audit")
+public class Inventory_Audit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long orderID;
+
+    private Long wareHouseID;
+
+    private Long productID;
+
+    private Long Quantity;
+
+    private String wareHouseLocation;
+
+    private Long ManufacturerID;
+
+
+
 }
